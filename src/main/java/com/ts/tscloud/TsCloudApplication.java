d2e639c4ddd06ed41bd948281ccc2f6dbb383ef5
Antaeseon <ridgnfu@naver.com>
2020-10-12 23:30:35 +0900

    first commit

---
+++ src/main/java/com/ts/tscloud/TsCloudApplication.java
@@ -0,0 +1,13 @@
+package com.ts.tscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsCloudApplication.class, args);
+    }
+
+}
