e06322b79c9c35d6420eeb60fc2eb064a5e218d3
Antaeseon <ridgnfu@naver.com>
2020-10-14 22:28:58 +0900

    chap 1 clear

---
+++ src/main/java/com/ts/tscloud/HomeController.java
@@ -0,0 +1,13 @@
+package com.ts.tscloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
